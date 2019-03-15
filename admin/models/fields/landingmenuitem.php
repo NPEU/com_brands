6e8c52f76fd16b818d9432933c346cb75a76c0f8
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2019-03-15 14:43:09 +0000

    Fixed landing menu item field to allow selection of existing item

--- admin/models/fields/landingmenuitem.php
+++ admin/models/fields/landingmenuitem.php
@@ -9,45 +9,47 @@
 
 defined('_JEXEC') or die;
 
+JFormHelper::loadFieldClass('menuitem');
 
 /**
  * Form field for a list of active staff members.
  */
-class JFormFieldLandingMenuItem extends JFormField
+class JFormFieldLandingMenuItem extends JFormFieldMenuitem
 {
     /**
      * The form field type.
      *
      * @var     string
      */
-    protected $type = 'LandingMenuItem';
+    public $type = 'LandingMenuItem';
 
-    /**
-     * Method to get the field input markup.
-     *
-     * @return  string  The field input markup.
-     */
-    protected function getInput()
-    {
-        $return = '';
-        
+	/**
+	 * Method to get the field input markup.
+	 *
+	 * @return  string  The field input markup.
+	 */
+	protected function getInput()
+	{
+       
         if (empty($this->value)) {
             
-            $return .= '<div class="control-label" style="width:auto;"><i>Reserved when first saved</i>';
+            //$return .= '<div class="control-label" style="width:auto;"><i>Reserved when first saved</i>';
+            return parent::getInput();
             
         } else {
             
-            $return .= '<div><a href="/administrator/index.php?option=com_menus&view=item&client_id=0&layout=edit&id=' . $this->value . '" target="_blank" class="btn  btn-primary">Edit Menu Item <span class="icon-out-2" aria-hidden="true"></span></a>';
-            
+            $return = array();
+            $return[] = '<div>';
+            $return[] = '    <a href="/administrator/index.php?option=com_menus&view=item&client_id=0&layout=edit&id=' . $this->value . '" target="_blank" class="btn  btn-primary">Edit Menu Item <span class="icon-out-2" aria-hidden="true"></span></a>';
+            $return[] = '</div>';
+            $return[] = '<input type="hidden" name="' . $this->name . '" value="' . $this->value . '">';
+            return implode("\n", $return);
         }
         
         // @TODO when possible, add a method to the related plugin to listen for menu item saves
         // that checks if the menu item is assigned to a project and, if so, update the projects
         // table with the type and/or link so that HERE we can check for if the menu item has been
         // properly set (and not just a heading) and then change the wording accordingly.
-        
-        //$return .= '<pre>' . print_r($this->element, true) .'</pre>';
-        $return .= '</div><input type="hidden" name="' . $this->name . '" value="' . $this->value . '">';
-        return $return;
-    }
+
+	}
 }
